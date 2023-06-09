becfd3328c54d4425aec7ca828b1c788084b67c2
unknown <raihanghani58@gmail.com>
2023-06-09 14:26:37 +0700

    fix dikit lagi

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/login", (req, res) => {
     res.redirect("/home");
   } else if (pilihan === "admin") {
     res.redirect("/admin");
-  } else {
+  } else if (pilihan === "guest") {
     res.redirect("/homebef");
   }
 });
